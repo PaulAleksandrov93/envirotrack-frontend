09fd224c9f061eb3bcac8eb14b38f6d520792f0f
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-09-01 20:40:14 +0500

    fixed some issues with create records

--- src/pages/ParametersPage.js
+++ src/pages/ParametersPage.js
@@ -271,7 +271,7 @@ const ParameterPage = () => {
   const [currentUser, setCurrentUser] = useState(null);
   const [measurementInstruments, setMeasurementInstruments] = useState([]);
   const { authTokens } = useContext(AuthContext);
-
+  const [selectedMeasurementInstrument, setSelectedMeasurementInstrument] = useState(null);
   const getCurrentUser = async () => {
     try {
       const response = await fetch(
@@ -354,11 +354,63 @@ const ParameterPage = () => {
     getMeasurementInstruments();
   }, [getParameter, getRooms, getMeasurementInstruments]);
 
+  // let createParameter = async () => {
+  //   if (currentUser) {
+  //     const newParameter = {
+  //       room: { room_number: selectedRoom.room_number },
+  //       measurement_instrument: { 
+  //         id: parameter.measurement_instrument.id,
+  //         name: parameter.measurement_instrument.name, 
+  //         type: parameter.measurement_instrument.type,
+  //         serial_number: parameter.measurement_instrument.serial_number,
+  //         calibration_date: parameter.measurement_instrument.calibration_date,
+  //         calibration_interval: parameter.measurement_instrument.calibration_interval,    
+  //       },
+  //       temperature_celsius: parameter.temperature_celsius,
+  //       humidity_percentage: parameter.humidity_percentage,
+  //       pressure_kpa: parameter.pressure_kpa,
+  //       pressure_mmhg: parameter.pressure_mmhg,
+  //       date_time: parameter.date_time,
+  //       responsible: {
+  //         id: currentUser.id,
+  //         first_name: currentUser.first_name,
+  //         last_name: currentUser.last_name,
+  //         patronymic: currentUser.patronymic
+  //       }
+  //     };
+  //     try {
+  //       const response = await fetch('/api/parameters/create/', {
+  //         method: 'POST',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: 'Bearer ' + String(authTokens.access),
+  //         },
+  //         body: JSON.stringify(newParameter),
+  //       });
+  
+  //       if (response.ok) {
+  //         navigate('/');
+  //       } else {
+  //         console.error('Failed to create parameter:', response.statusText);
+  //       }
+  //     } catch (error) {
+  //       console.error('Error while creating parameter:', error);
+  //     }
+  //   }
+  // };
+
   let createParameter = async () => {
     if (currentUser) {
       const newParameter = {
         room: { room_number: selectedRoom.room_number },
-        measurement_instrument: { id: parameter.measurement_instrument.id },
+        measurement_instrument: {
+          id: parameter.measurement_instrument.id,
+          name: parameter.measurement_instrument.name,
+          type: parameter.measurement_instrument.type,
+          serial_number: parameter.measurement_instrument.serial_number,
+          calibration_date: parameter.measurement_instrument.calibration_date,
+          calibration_interval: parameter.measurement_instrument.calibration_interval,
+        },
         temperature_celsius: parameter.temperature_celsius,
         humidity_percentage: parameter.humidity_percentage,
         pressure_kpa: parameter.pressure_kpa,
@@ -368,8 +420,8 @@ const ParameterPage = () => {
           id: currentUser.id,
           first_name: currentUser.first_name,
           last_name: currentUser.last_name,
-          patronymic: currentUser.patronymic
-        }
+          patronymic: currentUser.patronymic,
+        },
       };
       try {
         const response = await fetch('/api/parameters/create/', {
@@ -382,6 +434,7 @@ const ParameterPage = () => {
         });
   
         if (response.ok) {
+          setSelectedMeasurementInstrument(newParameter.measurement_instrument); // Обновляем СИ
           navigate('/');
         } else {
           console.error('Failed to create parameter:', response.statusText);
@@ -471,10 +524,31 @@ const ParameterPage = () => {
             options={measurementInstruments.map((instrument) => ({
               value: instrument.id,
               label: instrument.name,
+              type: instrument.type, // Добавляем параметры СИ
+              serial_number: instrument.serial_number,
+              calibration_date: instrument.calibration_date,
+              calibration_interval: instrument.calibration_interval,
             }))}
-            value={parameter && parameter.measurement_instrument ? { value: parameter.measurement_instrument.id, label: parameter.measurement_instrument.name } : null}
+            value={parameter && parameter.measurement_instrument ? {
+              value: parameter.measurement_instrument.id,
+              label: parameter.measurement_instrument.name,
+              type: parameter.measurement_instrument.type, // Добавляем параметры СИ
+              serial_number: parameter.measurement_instrument.serial_number,
+              calibration_date: parameter.measurement_instrument.calibration_date,
+              calibration_interval: parameter.measurement_instrument.calibration_interval,
+            } : null}
             onChange={(selectedOption) =>
-              setParameter((prevParameter) => ({ ...prevParameter, measurement_instrument: { id: selectedOption.value, name: selectedOption.label } }))
+              setParameter((prevParameter) => ({
+                ...prevParameter,
+                measurement_instrument: {
+                  id: selectedOption.value,
+                  name: selectedOption.label,
+                  type: selectedOption.type, // Добавляем параметры СИ
+                  serial_number: selectedOption.serial_number,
+                  calibration_date: selectedOption.calibration_date,
+                  calibration_interval: selectedOption.calibration_interval,
+                },
+              }))
             }
           />
         </div>
